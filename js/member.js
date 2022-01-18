998e30f92e5703f5bc32d44c5f3ec4a14596aa14
Zidiefeng <35935604+Zidiefeng@users.noreply.github.com>
2022-01-17 18:01:59 -0800

    Update member.js

--- js/member.js
+++ js/member.js
@@ -1,26 +1,50 @@
 var member = [
   {
-    "phone": "No.2038",
+    "phone": "No.1",
     "name": "1"
   },
   {
-    "phone": "No.2038",
+    "phone": "No.2",
     "name": "2"
   },
   {
-    "phone": "No.2038",
+    "phone": "No.3",
     "name": "3"
   },
   {
-    "phone": "No.2038",
+    "phone": "No.4",
     "name": "4"
   },
   {
-    "phone": "No.2038",
+    "phone": "No.5",
     "name": "5"
   },
   {
-    "phone": "No.2038",
+    "phone": "No.6",
+    "name": "6"
+  },
+  {
+    "phone": "No.7",
+    "name": "6"
+  },
+  {
+    "phone": "No.8",
+    "name": "6"
+  },
+  {
+    "phone": "No.9",
+    "name": "6"
+  },
+  {
+    "phone": "No.10",
+    "name": "6"
+  },
+  {
+    "phone": "No.11",
+    "name": "6"
+  },
+  {
+    "phone": "No.12",
     "name": "6"
   }
 ]
